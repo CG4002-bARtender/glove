40033818b8907f9804f77dee583d905d43a00b47
thiru <thiruvageesan3@gmail.com>
2026-02-07 21:25:22 +0800

    revert blink script

--- src/scripts/blink.cpp
+++ src/scripts/blink.cpp
@@ -5,20 +5,15 @@ void setup()
 {
   Serial.begin(config::kBaudRate);
   pinMode(config::kLedPin, OUTPUT);
-  pinMode(config::kRecordButtonPin, INPUT_PULLUP);
-  Serial.println("=== Button Test ===");
-  Serial.printf("Button pin: GPIO %d (INPUT_PULLUP)\n", config::kRecordButtonPin);
-  Serial.println("Press button to toggle LED...");
 }
 
 void loop()
 {
-  bool pressed = !digitalRead(config::kRecordButtonPin);
-  digitalWrite(config::kLedPin, pressed ? HIGH : LOW);
+  Serial.print("ON");
+  digitalWrite(config::kLedPin, HIGH);
+  delay(config::kBlinkDelayMs);
 
-  if (pressed)
-  {
-    Serial.println("PRESSED");
-    delay(200);
-  }
+  Serial.println("OFF");
+  digitalWrite(config::kLedPin, LOW);
+  delay(config::kBlinkDelayMs);
 }
